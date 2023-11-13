feb3772c9dbc2c2f2dccc5866eef69988420fb42
7ras0tresh <maximandreychuk1999@gmail.com>
2023-11-13 12:25:13 +0300

    todo is complete

--- todo/todolist/todoapp/urls.py
+++ todo/todolist/todoapp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'), 
+    path('add', views.add_todo, name = 'add'),
+    path('update/<int:todo_id>/', views.update_todo, name = 'update'),
+    path('delete/<int:todo_id>/', views.delete_todo, name = 'delete')
 ]
--- todo/todolist/todoapp/views.py
+++ todo/todolist/todoapp/views.py
@@ -1,4 +1,31 @@
-from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render, redirect
+from .models import ToDo
+
 
 def index(request):
-    return HttpResponse('HelloWorld')
+
+    todo_lst = ToDo.objects.all()
+    return render(request, 'todoapp/index.html', 
+                  {'todo_lst_key': todo_lst,
+                   'title': 'Главная страница'})
+
+@require_http_methods(['POST'])
+def add_todo(request):
+
+    title = request.POST['title']
+    todo = ToDo(title=title)
+    todo.save()
+    return redirect('index')
+
+def update_todo(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect('index')
+
+def delete_todo(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)
+    todo.delete()
+    # todo.save()
+    return redirect('index')
